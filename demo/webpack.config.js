5abd467b53a1bf429710926023f38fca1bf191fc
j0ker <j0ker@desktop3.desktop3>
2021-02-08 10:27:31 -0800

    multiple ports

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
     },
     module: {
         rules: [
+		{
+    test: /\.js$/,
+    enforce: 'pre',
+    use: ['source-map-loader'],
+  },
             {
                 test: /\.worker\.js$/,
                 use: {loader: 'worker-loader'}
